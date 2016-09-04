7e7813faf1c7ff88dd48f857bb72d9321bd1da7a
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2016-09-04 15:44:34 +0800

    fix

--- apps/base/admins/__init__.py
+++ apps/base/admins/__init__.py
@@ -21,7 +21,7 @@ class SipInterfaceAdmin(ReXmlAdmin):
     
     def get_nav_btns(self):
         return [
-            '''<a href="/xadmin/page/sipinterfacemodels/" class="btn btn-primary"><i class="fa fa-inbox"></i> 自动检测</a> '''
+            '''<a href="/xadmin/page/sipinterfaceautoconf/" class="btn btn-primary"><i class="fa fa-inbox"></i> 自动检测</a> '''
         ]
 
 site.register(models.SipInterface, SipInterfaceAdmin)
