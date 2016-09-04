4988119e05ea6eb0cd683506f6d6a1e7d964808b
jone@meilapp.com <jone@meilapp.com>
2016-09-04 13:08:06 +0800

    add config init_data

---
+++ config.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+
+fs_conf_path = 'C:/Program Files (x86)/FreeSWITCH/conf'#'/usr/local/freeswitch/conf'
+
+odbc_credentials = "freepybx:freepybx:secretpass1"
+
+
+event_socket_conf = {
+                     'host': '127.0.0.1',
+                     'port': 8021,
+                     'pwd': '520999'
+                     }
+
+fs_db_path = 'C:/Program Files (x86)/FreeSWITCH/db'
+
+fs_sounds_path = ''
