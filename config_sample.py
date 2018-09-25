a2165a71f48ddc2bd35dfcfbb50ae4f2cecea32e
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2018-09-25 16:57:30 +0800

    config优化，使用时从config_sample重命名

---
+++ config_sample.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+
+fs_conf_path = '/usr/local/freeswitch/conf/'
+
+odbc_credentials = "freepybx:freepybx:secretpass1"
+
+
+event_socket_conf = {
+                     'host': '127.0.0.1',
+                     'port': 8021,
+                     'pwd': ''
+                     }
+
+fs_db_path = '/usr/local/freeswitch/db/'
+
+fs_sounds_path = '/usr/local/freeswitch/sounds'
+
+main_server_conf = {
+    'host': '0.0.0.0',
+    'port': 8080
+}
