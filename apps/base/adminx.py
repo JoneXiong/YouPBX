44f3c277d4dcbfc24e3f51507c67b4f693afc150
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2018-09-26 14:23:11 +0800

    默认使用bootstrap3原生UI

--- apps/base/adminx.py
+++ apps/base/adminx.py
@@ -8,13 +8,14 @@ from xadmin.views.dashboard import AppDashboard
 site.site_title = 'YouPBX'
 site.site_footer  = 'Oejia CopyRight'
 site.menu_style = 'default'
+site.ext_ui = False
 
 import dashboards
 
 class BaseIndex(AppDashboard):
     app_label = 'base'
     #widget_customiz = False
-    
+
 site.register_appindex(BaseIndex)
 
 import admins
