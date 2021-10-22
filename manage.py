e24a8b74814761bc90fd86f4217c92ec7238874b
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2021-10-22 22:47:05 +0800

    增加缺失的执行内容

--- manage.py
+++ manage.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 import os
 import sys
+import subprocess
 
-import gevent.monkey
-gevent.monkey.patch_all()
+from config import event_socket_conf as fsc
 
 if __name__ == "__main__":
     PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
     sys.path.insert(0, os.path.join(PROJECT_ROOT, 'lib'))
+    if 'runserver' in sys.argv:
+        if sys.platform in ['win32', 'cygwin']:
+            cmd = './proxy/win/fs_agw'
+        else:
+            cmd = './proxy/linux/fs_agw'
+        p = subprocess.Popen(cmd + ' -fshost %s -fsport %s -pass %s' % (fsc['host'], fsc['port'], fsc['pwd']))
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apps.settings")
 
